ad96d3eb3f5d9206c32d31c8b144f2272b0c482b
imjszhang <ortle3x3@gmail.com>
2024-12-14 16:31:09 +0800

    添加数据库导出功能，允许用户将本地数据库导出为 JSON 文件

--- pages/dashboard/analytics.vue
+++ pages/dashboard/analytics.vue
@@ -5,6 +5,7 @@
     </Teleport>
     <div class="flex flex-1 p-6 overflow-scroll">
       <p>本地数据库占用约为 <span class="text-rose-500">{{usage}}</span></p>
+      <button @click="exportDatabase" class="mt-4 p-2 bg-blue-500 text-white rounded">导出数据库</button>
     </div>
   </div>
 </template>
@@ -23,4 +24,45 @@ async function init() {
 }
 
 await init()
+
+async function exportDatabase() {
+  const dbs = await indexedDB.databases();
+  for (const dbInfo of dbs) {
+    const db = await openDatabase(dbInfo.name!, dbInfo.version!);
+    const exportData = await exportToJson(db);
+    const blob = new Blob([JSON.stringify(exportData)], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = `${dbInfo.name}.json`;
+    a.click();
+    URL.revokeObjectURL(url);
+  }
+}
+
+function openDatabase(name: string, version: number): Promise<IDBDatabase> {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open(name, version);
+    request.onsuccess = () => resolve(request.result);
+    request.onerror = () => reject(request.error);
+  });
+}
+
+function exportToJson(db: IDBDatabase): Promise<any> {
+  return new Promise((resolve, reject) => {
+    const transaction = db.transaction(db.objectStoreNames, 'readonly');
+    const exportData: any = {};
+    let count = db.objectStoreNames.length;
+
+    for (const storeName of db.objectStoreNames) {
+      const store = transaction.objectStore(storeName);
+      const request = store.getAll();
+      request.onsuccess = () => {
+        exportData[storeName] = request.result;
+        if (--count === 0) resolve(exportData);
+      };
+      request.onerror = () => reject(request.error);
+    }
+  });
+}
 </script>
